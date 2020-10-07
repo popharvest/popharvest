d047bf5747ad64537c406b1f2a549f60cc5d2bd4
popharvest <53858098+popharvest@users.noreply.github.com>
2020-10-07 20:40:08 +0200

    Add files via upload

---
+++ devtools_history.R
@@ -0,0 +1,8 @@
+usethis::use_build_ignore("devtools_history.R")
+usethis::use_build_ignore("dependencies.R")
+usethis::use_vignette("README")
+usethis::use_package("stats")
+usethis::use_package("ggplot2")
+usethis::use_package("gridExtra")
+usethis::use_package("kableExtra")
+usethis::use_package("stringr")
