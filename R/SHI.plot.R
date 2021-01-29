2e110de25b5dc4012337e5242cdbf00787d993c0
popharvest <53858098+popharvest@users.noreply.github.com>
2021-01-29 19:57:47 +0100

    Add files via upload

---
+++ R/SHI.plot.R
@@ -0,0 +1,141 @@
+#' Sustainability harvest index visualizations from PEG and PTL functions
+#'
+#' Create data visualizations of sustainability harvest index from \code{PEG} and \code{PTL} functions
+#'
+#' @param data dataframe from \code{PEG} and \code{PTL} functions output. Other inputs will give an error.
+#' @param Sp number ; the identification number assigned to the studied species
+#' @param IDSp boolean ; default \code{TRUE}. If \code{TRUE}, print the identification number assigned to the studied species. If \code{FALSE}, doesn't print the identification number assigned to the studied species.
+#' @param NameSp character string ; the name of the studies species. Add the names of the studies species to the title of the graphic.
+#' @param title character string ; default "Distribution of sustainability harvest index" ; title plot
+#' @param title.x character string ; default "Sustainability harvest index" ; title axis x
+#' @param title.y character string ; default "Simulations number" ; title axis y
+#' @param color1 character string ; default green (#008000) ; the first colour that the user wants for the graphic
+#' @param color2 character string ; default red (#B40000) ; the second colour that the user wants for the graphic
+#'
+#' @return data visualisations
+#' @export
+#' @seealso \code{\link{PEG}}, \code{\link{PTL}}.
+#' @import ggplot2 gridExtra
+#' @examples
+#' df <- PEG(Nsim = 20, NSp = 2, living.rate = c("long", "short"),
+#' surv.fixed = c(0.8, 0.65),
+#' alpha.unif = TRUE, min.alpha = c(2, 1), max.alpha = c(3, 2),
+#' pop.fixed = c(3605244, 55805898),
+#' harvest.fixed = c(107802, 8447950),
+#' Fs = c(0.1, 0.3, 0.5))
+#'
+#' SHI.plot(df)
+SHI.plot <- function(data = NULL,
+                     Sp = NULL,
+                     IDSp = TRUE,
+                     NameSp = NULL,
+                     title = "Distribution of sustainability harvest index",
+                     title.x = "Sustainability harvest index",
+                     title.y = "Number of simulations",
+                     color1 = "#00BFC4",
+                     color2 = "#F8766D"
+){
+
+  ## I. Error messages ----
+  # Print error messages if the vector types of the datas are not correct
+  if(is.null(data)){ # If 'data' argument is null then print error message
+    stop("'data' must be specified")
+  }
+  if (!is.data.frame(data)) { # If 'data' argument is not a data.frame then print error message
+    stop("'data' must be a dataframe")
+  }
+  if(!is.null(Sp) && !is.numeric(Sp)){ # If 'Sp' argument is not null and is not a numeric vector then print error message
+    stop("'Sp' must be a numeric vector")
+  }
+  if(!is.null(NameSp) && !is.character(NameSp)){ # If 'NameSp' argument is not null and is not a character string then print error message
+    stop("'NameSp' must be a character string")
+  }
+
+  ## II. Creation of graphs ----
+
+  ## Step 1 : Filter of datas ----
+  # This step allows to create a graph for the species wanted
+  if(!is.null(Sp)){ # If 'Sp' is not null then the data frame named 'data' is filtered according to 'Sp'
+    dataFilterLogical <- with( # Create a data filter logical compararing the values of the column 'SP' of the data frame 'data' and the value of the object 'Sp'
+      data,
+      SP %in% Sp
+    )
+    newdata <- data[dataFilterLogical, ] # Extract the data frame for which 'dataFilterLogical' is TRUE
+  } else { newdata <- data } # If 'Sp' is null then the data frame 'data' is renamed 'newdata'
+
+  newdata$SP <- factor(newdata$SP) # Convert columns 'SP' of the data frame named 'newdata' to a factor vector
+
+
+
+  ## Step 2 :  Right legend and facet boxes
+  # This step allows to write the right legend for the facet boxes according to the PEG or PTL function used previously.
+  # It also simplifies the creation of the facet boxes by rename the column "Fs" or "Fobj" in a unique column named "param".
+  col <- which(colnames(data) == "Fs") # Search for a column named "Fs"
+
+
+
+  ## Condition "if" : If the object 'col' is > 0 ----
+  # There is a column named "Fs" in the data frame 'data' and the PEG function was used previously
+  if(length(col)> 0){
+    legend <- "Safety factor" # If the function used previously is PEG then the legend of the facet boxes is "Safety factor"
+    colnames(newdata)[which(names(newdata)=="Fs")] <- "param" # Rename the column "Fs" by "param"
+
+
+
+    ## Condition "else" : If the object 'col' is not > 0 ----
+    # There is a column named "Fobj" in the data frame 'data' and the PTL function was used previously
+  } else {
+    legend <- "Objective factor" # If the function used previously is PTL then the legend of the facet boxes is "Objective factor"
+    colnames(newdata)[which(names(newdata)=="Fobj")] <- "param" # Rename the column Fobj" by "param"
+  }
+
+
+  ## Step 3 : Data visualizations ----
+  # This step allows to create the plot from a loop according to the level 'SP'
+  for(i in levels(newdata$SP)){
+
+    newdataT <- subset(newdata, newdata$SP == i) # Extract the data frame for which the value of the 'SP' column is equal to the level i. It allows to create a plot by 'SP'
+
+    index <- as.numeric(i) # Convert the index i as a numeric vector for the next line
+    Species <- NameSp[index] # Extract the name of the species study if it is given
+    if (is.null(NameSp)){
+      addtitle <- NULL
+    } else {addtitle <- paste0(" of ",Species)} # Additionnal title for the title of the graphic with the name of the study species
+
+    if (isTRUE(IDSp)){
+      main.title <- paste0("SP", i, "\n",title, addtitle, "\n")
+    } else { main.title <- paste0(title, addtitle, "\n") }
+
+    nb_ticks <- function(n) {function(limits) pretty(limits, n)} # Function which allows to adapt the scale of the horizontal axis  according to the datas
+    newdataT$high_rev <- factor((newdataT[,"SHI"]>1)*1) # The new column named "high_rev" allows to adapt the colour during the creation of the plot according to if SHI is lower or upper than 1
+
+
+    theme_set(theme_classic()) # The theme of the plot
+
+    print(
+      ggplot(newdataT, aes_(x=~SHI, fill=~high_rev)) + # Plot with values of the column 'SHI' in horizontal axis from the data frame 'newdataT' and colorization according to the values of the column 'high_rev'
+        geom_histogram(position="identity", colour="grey15", bins=50) + # Create an histogram
+        facet_grid(param ~.) + # Facet according to the values of the column "Fs"
+        geom_vline(aes(xintercept = 1), color="black", linetype="dashed", size = 0.7) + # Dotted line at 1
+        labs(title= main.title , x=paste0("\n", title.x), y= paste0(title.y,"\n"), subtitle=legend) + # Main title, legend title and axes title
+        scale_fill_manual(values=c(color1, color2)) + # Change the graph colours according to the values of "high_rev"
+        scale_x_continuous(breaks=nb_ticks(10)) + # Change the scale of the horizontal axis
+        theme(plot.subtitle = element_text(hjust = 1.05), # Position of facets legend
+              panel.border = element_blank(), # No borders for facet boxes
+              panel.grid.minor.y = element_blank(), # No background graduation
+              panel.grid.major.x = element_blank(), # No background graduation
+              panel.grid.minor.x = element_blank(), # No background graduation
+              panel.spacing = unit(1.2, "lines"), # Change the spacing between facets
+              plot.title = element_text(colour = "grey15", hjust = 0.5, size = 16), # Changes the title plot
+              axis.text = element_text(size=11, color = "grey15"), # Changing the axes text
+              axis.title = element_text(size=13, color = "grey15"), # Changing the axes title
+              legend.background = element_blank(), # No background for the legend
+              legend.position="none" , # No legend
+              strip.background = element_rect(color="grey80", fill="white", size=0.5, linetype="solid"), # Change the facets box
+              strip.text = element_text(color = "grey15", size = 11)) # Change the facets text
+    )
+
+  } # End of loop i
+
+
+} # End of function
---
+++ R/rmax.R
@@ -0,0 +1,189 @@
+#' Maximum annual recruitment rate estimation
+#'
+#' \code{rmax} is a function used to estimate the maximum annual recruitment rate, see 'Details'.
+#'
+#' @details
+#' When the maximum annual recruitment rate, Rmax, is unknown from the user,
+#' the value can be calculated using the Demographic Invariants Method (DIM) (Niel & Lebreton, 2005). This approach calculates \eqn{Rmax = lambdaMax – 1} (Dillingham & Fletcher, 2008) on the basis of 2 parameters:
+#' adult survival rate (\code{surv} arguments) and age at first breeding \code{alpha} arguments). When adult survival and age at first breeding are known
+#' from the user, they can be provided by either point estimates (\code{surv.fixed}, \code{alpha.fixed} arguments) or distributions (\code{surv.beta = TRUE} with \code{mean.surv} and \code{sd.surv},
+#' \code{alpha.unif = TRUE} with \code{min.alpha} and \code{max.alpha}, \code{alpha.lognorm = TRUE} with \code{mean.alpha} and \code{sd.alpha} arguments). Also,
+#' adult survival rate can be estimated from body mass following the relationship described in Johnson et al. (2012) : \eqn{p^(1/(exp(3.22+0.24*log(body mass)+e)-alpha))} (see \code{surv} function).
+#' In this case, body mass can be provided as either a point estimate (\code{mass.fixed})
+#' or a log-normal distribution (\code{mass.lognorm = TRUE}, \code{mean.mass} and \code{sd.mass} arguments). The parameter p can be provided by either from a beta distribution
+#' B(3.34, 101.24) (\code{type.p = "random"}) or a point estimate equal to 0.032 (i.e. the mean of the beta distribution is given by a/(a + b) with a = 3.34 and b = 101.24, Boitani & Fuller 2000) (\code{type.p = "determinist"})
+#' The parameter e can also be either null (\code{type.e = "determinist"}) or residuals from a normal distribution N(0, 0.087) (\code{type.e = "random"}).
+#'
+#' @param Nsim number, default 1 ;  number of simulations
+#' @param living.rate character string ; living rate : either "\code{long}" or "\code{short}". No other character string allowed.
+#' @param surv.fixed number ; fixed estimate of adult annual survival rate provided by the user. To be used instead of \code{mean.surv} when \code{surv.beta = FALSE}.
+#' @param surv.beta surv.beta boolean, default FALSE. If \code{TRUE}, simulates adult annual survival rate from a beta distribution with mu = \code{mean.surv} and standard deviation = \code{sd.surv} (see below).
+#' @param mean.surv mean.surv number ; arithmetic mean of adult annual survival rate. Only used if \code{surv.beta = TRUE}.
+#' @param sd.surv sd.surv number ; standard deviation of adult annual survival rate. Only used if \code{surv.beta = TRUE}.
+#' @param alpha.fixed alpha.fixed number ; fixed estimate of age at first breeding (in years). To be used when \code{alpha.unif = FALSE} and \code{alpha.lognorm = FALSE}.
+#' @param alpha.unif  alpha.unif boolean, default \code{FALSE}. Simulates age at first breeding from a uniform distribution bounded by \code{min.alpha} and \code{max.alpha}.
+#' @param min.alpha  min.alpha number ; minimum age at first breeding. To be used only if \code{alpha.unif = TRUE}.
+#' @param max.alpha  max.alpha number ; maximum age at first breeding. To be used only if \code{alpha.unif = TRUE}.
+#' @param alpha.lognorm  alpha.lognorm boolean, default \code{FALSE}. If \code{TRUE}, simulates age at first breeding from a normal distribution with mu = \code{mean.alpha} and standard deviation = \code{sd.alpha}.
+#' @param mean.alpha mean.alpha number ; arithmetic mean of age at first breeding. To be used only used if \code{alpha.lognorm = TRUE}.
+#' @param sd.alpha sd.alpha number ; age at first breeding standard deviation. Only used if \code{alpha.lognorm = TRUE}.
+#' @param mass.fixed mass.fixed number ; fixed estimate of body mass (in kilograms) used to estimate adult survival rate from body mass.
+#' @param mass.lognorm  mass.lognorm boolean, default \code{FALSE}. If \code{TRUE}, simulates body mass from a normal distribution with mu = \code{mean.mass} and standard deviation = \code{sd.mass}.
+#' @param mean.mass  mean.mass number ; arithmetic mean of body mass (in kilograms). Only used if \code{mass.lognorm = TRUE}. #' @param sd.mass number ; standard deviation of body mass. Only used if \code{mass.lognorm = TRUE}.
+#' @param sd.mass sd.mass number ; standard deviation of body mass. Only used if \code{mass.lognorm = TRUE}.
+#' @param type.p type.p character string ; the value of the parameter p can be either "\code{determinist}" or "\code{random}". No other character string allowed.
+#' @param type.e type.e character string ; the value of the parameter e can be either "\code{determinist}" or "\code{random}". No other character string allowed.
+#' @param surv.j surv.j number, default 1 ; fixed estimate of juvenile annual survival rate provided by the user. It is used to estimate adult survival rate from body mass.
+
+#'
+#' @return A dataframe
+#' @export
+#' @seealso \code{\link{PEG}}, \code{\link{PTL}}.
+#' @references
+#' Dillingham, P. W., & Fletcher, D. (2008). Estimating the ability of birds to sustain additional human-caused mortalities using a simple decision rule and allometric relationships. Biological Conservation, 141(7), 1783-1792.
+#'
+#' Johnson, F. A., Walters, M. A., & Boomer, G. S. (2012). Allowable levels of take for the trade in Nearctic songbirds. Ecological Applications, 22(4), 1114-1130.
+#'
+#' Niel, C., & LEBRETON, J. D. (2005). Using demographic invariants to detect overharvested bird populations from incomplete data. Conservation Biology, 19(3), 826-835.
+#'
+#' @importFrom stats rlnorm rnorm rbeta
+#'
+rmax <- function (Nsim = 1,
+                  living.rate = NULL,
+                  surv.fixed = NULL,
+                  surv.beta = FALSE,
+                  mean.surv = NULL,
+                  sd.surv = NULL,
+                  mass.fixed = NULL,
+                  mass.lognorm = FALSE,
+                  mean.mass = NULL,
+                  sd.mass = NULL,
+                  type.p = NULL,
+                  type.e = NULL,
+                  surv.j = 1,
+                  alpha.fixed = NULL,
+                  alpha.unif = FALSE,
+                  min.alpha = NULL,
+                  max.alpha = NULL,
+                  alpha.lognorm = FALSE,
+                  mean.alpha = NULL,
+                  sd.alpha = NULL) {
+
+  ## Step 1 : Create end storage vector to save the results of the function and load functions ----
+  Output <- NULL
+  lambdaMax.calc <- NULL
+
+  # lognormal distribution (source : Fred Johnson)
+  MOM.lognorm <- function(expect,stdev){
+    sig <- log(1+(stdev^2/expect^2))
+    se <- sqrt(sig)
+    est <- log(expect)-(sig*0.5)
+    result <- c(est,se)
+    return(result)
+  }
+
+  # beta distribution (source : Fred Johnson)
+    MOM.beta <- function(mu,var) {
+    sum_ab <- (mu*(1-mu)/var)-1
+    a <- sum_ab*mu
+    b <- sum_ab*(1-mu)
+    beta <- c(a, b)
+    return(beta)
+  }
+
+
+
+  # Step 2 : Generate estimated variables ----
+  ## alpha (age at the first breeding) estimation ----
+  if(isTRUE(alpha.unif)) { # If alpha is extracted from  a uniform distribution then :
+    alpha.calc <- runif(Nsim, min.alpha, max.alpha) # 'Nsim' values are extracted from a uniform distribution with 'min.alpha' as lower bound and 'max.alpha' as upper bound
+  }
+  if(isTRUE(alpha.lognorm)) { # If alpha is extracted from  a normal distribution then :
+    alpha.param <- MOM.lognorm(mean.alpha, sd.alpha)
+    alpha.calc <- rlnorm(Nsim, alpha.param[1], alpha.param[2]) # 'Nsim' values are extracted from a log-normal distribution with with the mean 'mean.alpha' and the standard deviation 'sd.alpha'
+  }
+  if(!is.na(alpha.fixed)) {
+    alpha.calc <- rep(alpha.fixed, Nsim)
+  }
+  if(!isTRUE(alpha.unif) && !isTRUE(alpha.lognorm) && is.na(alpha.fixed) && !is.na(max.alpha)){
+    alpha.calc <- rep(max.alpha, Nsim)
+  }
+
+
+
+
+  ## survival estimation ----
+  if(!is.na(surv.fixed)){ # If the value of surv.fixed is different to 'NA' (i.e. the value of surv.fixed is known) then :
+    survival.calc <- rep(surv.fixed, Nsim) # The value of surv.fixed is replicated 'Nsim' times and saves in the object 'survival.calc'
+  }
+  if(!is.na(mass.fixed)){ # If the value of mass.fixed is different to 'NA' (i.e. the value of the body mass is known) then :
+    survival.tab <- surv(Nsim, mass.fixed = mass.fixed, alpha = alpha.calc, type.p = type.p, type.e = type.e, surv.j = surv.j) # The values of survival are calculated from the surv function and save in a data frame named 'survival.tab'
+    survival.calc <- survival.tab$survival # The values of survival are extract from the data frame survival.tab and save in the object 'survival.calc'
+  }
+  if(isTRUE(mass.lognorm)){ # If the body mass is extracted from a normal distribution then :
+    survival.tab <- surv(Nsim, mass.lognorm = TRUE, mean.mass = mean.mass, sd.mass = sd.mass, alpha = alpha.calc, type.p = type.p, type.e = type.e, surv.j = surv.j) # The values of survival are calculated from the surv function and save in a data frame named 'survival.tab'
+    survival.calc <- survival.tab$survival # The values of survival are extract from the data frame survival.tab and save in the object 'survival.calc'
+  }
+  if(isTRUE(surv.beta)){ # If survival is extracted from a normal distribution then :
+    beta.param <- MOM.beta(mean.surv, sd.surv^2)
+    survival.calc <- rbeta(Nsim, beta.param[1], beta.param[2]) # 'Nsim' values are extracted from a beta distribution
+    survival.calc[survival.calc > 1] <- 0.9999999
+    rm(beta.param)
+  }
+
+
+
+
+  ## lambdaMax estimation ----
+  if(living.rate == "short"){ # If the species studied is a short-lived species then :
+    for (b in 1:Nsim){ # Loop for calculations from 1 to the number of iterations wanted
+      alpha.calcT <- alpha.calc[b] # Temporary setting the value of the object alpha.calc of the index b
+      survival.calcT <- survival.calc[b] # Temporary setting the value of the object survival.calc of the index b
+      lambdaMax <- lambdaMax.S(alpha.calcT,survival.calcT) # Use the lambdaMax.S function which makes it possible the lamndaMax estimation for a short-lived species
+      lambdaMax.calc <- c(lambdaMax.calc, lambdaMax)
+    } # End of loop b
+  } else { # If the species is not a short-lived species then it implies it is a long-lived species
+    lambdaMax.calc <- lambdaMax.L(alpha.calc, survival.calc) # Use of the lambdaMax.L function which makes it possible the lamndaMax estimation for a long-lived species
+  }
+
+
+
+
+  ## Rmax estimation ----
+  Rmax <-  lambdaMax.calc - 1
+
+
+
+  ## Step 3 : Save datas generated before in a dataframe named 'Output'---- ----
+  NsimT = 1:Nsim # This object makes it possible to known the value of the index a for each row of the data frame below
+  Output <- data.frame(Nsim = NsimT, Rmax = Rmax, lambdaMax = lambdaMax.calc, living.rate = living.rate,
+                        survival = survival.calc, alpha = alpha.calc)  # Create the Output
+
+
+
+  ## Step 4 : Complementary columns for the 'Output' ----
+  # This step integrates columns in the final data frame according to the arguments informed
+  var.storage <- c("min.alpha", "max.alpha","mean.alpha", "sd.alpha", "mass.fixed",
+                   "mean.mass","sd.mass", "surv.j") # Save in an object all sumpplementary possible columns
+  not.na <- which(!is.na(unlist(mget(var.storage)))) # Select columns whose values are different from NA. Works only with NA. That is why parameters which are NULL are transformed as NA
+  Output[,var.storage[not.na]]<- mget(var.storage)[not.na] # Insert values columns whose values are different from NA in the data frame 'Output'
+  if(exists("survival.tab")){Output$mass <- survival.tab$mass} # If the data frame 'survival.tab' exists then add the mass values
+  if(exists("survival.tab")){Output$e <- survival.tab$e} # If the data frame 'survival.tab' exists then add the e values
+  if(exists("survival.tab")){Output$p <- survival.tab$p} # If the data frame 'survival.tab' exists then add the p values
+
+
+
+  ## Step 5 : Reorder columns of the 'Output'----
+  var.surv <- NULL
+  if(exists("survival.tab")){var.surv <- c(var.surv, "mass", "e", "p", "surv.j")} # Order of the columns wanted in the final output
+  var.surv.name <- var.surv[var.surv %in% colnames(Output)] # Reorder the columns in the data frame named 'Output'
+
+  var.alpha <- c("min.alpha", "max.alpha","mean.alpha", "sd.alpha") # Order of the columns wanted in the final output
+  var.alpha.name <- var.alpha[var.alpha %in% colnames(Output)] # Reorder the columns in the data frame named 'Output'
+
+  reordering <- c("Nsim", "Rmax", "living.rate", "lambdaMax", "survival", var.surv.name, "alpha", var.alpha.name) # Final order of the columns wanted in the final output
+  Output <- data.frame(Output[, reordering]) # Reorder the columns in the data frame named 'Output'
+
+
+
+} # End of the function
