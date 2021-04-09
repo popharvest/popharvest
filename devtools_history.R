e4a4e06ad6a873b7f0791eaac2ee153a2df4184c
popharvest <53858098+popharvest@users.noreply.github.com>
2021-04-09 11:50:09 +0200

    Delete devtools_history.R

--- devtools_history.R
+++
@@ -1,8 +0,0 @@
-usethis::use_build_ignore("devtools_history.R")
-usethis::use_build_ignore("dependencies.R")
-usethis::use_vignette("README")
-usethis::use_package("stats")
-usethis::use_package("ggplot2")
-usethis::use_package("gridExtra")
-usethis::use_package("kableExtra")
-usethis::use_package("stringr")
